cbf49237efb3b844b857a6b3df2abe5f34a60170
Charan <charansai7890@gmail.com>
2023-07-30 17:49:15 +0530

    combine params into single type (tuple)

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -41,7 +41,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
         var deliveredErrors = [RemoteFeedLoader.Error]()
         sut.load() { deliveredErrors.append($0) }
-        
+
         let clientError = NSError(domain: "Test", code: 0)
         client.complete(with: clientError)
 
@@ -56,16 +56,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
 
     class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
-        var completions = [(Error) -> Void]()
+        var requestedURLs: [URL] {
+            messages.map { $0.url }
+        }
+        private var messages = [(url: URL, completion: ((Error) -> Void))]()
 
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            requestedURLs.append(url)
-            completions.append(completion)
+            messages.append((url, completion))
         }
 
         func complete(with error: Error, at index: Int = 0) {
-            completions[index](error)
+            messages[index].completion(error)
         }
     }
 }
