7c5e3c15bfddeabbb8299a655fd89a630ab265b2
Charan <charansai7890@gmail.com>
2023-08-01 23:11:08 +0530

    added memory tracking

--- EssentialFeedTests/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/URLSessionHTTPClientTests.swift
@@ -68,8 +68,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
 
     //MARK: Helpers
 
-    private func makeSUT() -> URLSessionHTTPClient {
-        URLSessionHTTPClient()
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
+        let sut = URLSessionHTTPClient()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
     }
 
     private func anyURL() -> URL {
