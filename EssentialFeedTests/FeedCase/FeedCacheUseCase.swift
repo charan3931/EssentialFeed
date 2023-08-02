a4540cee071f9c8a0c2deb8e8a385236eedc671a
Charan <charansai7890@gmail.com>
2023-08-02 23:03:00 +0530

    added memory tracking

--- EssentialFeedTests/FeedCase/FeedCacheUseCase.swift
+++ EssentialFeedTests/FeedCase/FeedCacheUseCase.swift
@@ -71,9 +71,11 @@ final class FeedCacheUseCase: XCTestCase {
 
     //MARK: Helpers
 
-    func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+    func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
         let store = FeedStore()
         let sut = LocalFeedLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
     }
 }
