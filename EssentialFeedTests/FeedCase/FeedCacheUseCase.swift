c26256fb4fc4929020f77b6b466b60043d6e1ad9
Charan <charansai7890@gmail.com>
2023-08-03 00:27:58 +0530

    make access control for makeSUT as private

--- EssentialFeedTests/FeedCase/FeedCacheUseCase.swift
+++ EssentialFeedTests/FeedCase/FeedCacheUseCase.swift
@@ -125,7 +125,7 @@ final class FeedCacheUseCase: XCTestCase {
 
     //MARK: Helpers
 
-    func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
         let store = FeedStore()
         let sut = LocalFeedLoader(store: store)
         trackForMemoryLeaks(store, file: file, line: line)
