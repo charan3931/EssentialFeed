7e8d02c37a915245e8719706701f7c9d8e32ee1c
Charan <charansai7890@gmail.com>
2023-08-02 22:48:50 +0530

    LocalFeedLoader deletes the saved cache on save

--- EssentialFeedTests/FeedCase/FeedCacheUseCase.swift
+++ EssentialFeedTests/FeedCase/FeedCacheUseCase.swift
@@ -9,18 +9,40 @@ import XCTest
 
 class LocalFeedLoader {
 
+    let store: FeedStore
+
+    init(store: FeedStore) {
+        self.store = store
+    }
+
+    func save() {
+        store.deleteCache()
+    }
 }
 
 class FeedStore {
     var deletionCount = 0
+
+    func deleteCache() {
+        deletionCount += 1
+    }
 }
 
 final class FeedCacheUseCase: XCTestCase {
 
     func test_init_doesNotDeleteCache() {
-        let sut = LocalFeedLoader()
         let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
 
         XCTAssertTrue(store.deletionCount == 0)
+    }
+
+    func test_save_deletesCache() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+
+        sut.save()
+
+        XCTAssertTrue(store.deletionCount == 1)
     }
 }
