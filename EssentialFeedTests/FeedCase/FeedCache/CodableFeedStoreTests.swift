343f4c5d749fbb3783061d7a51d727910359fb22
Charan <charansai7890@gmail.com>
2023-08-04 21:35:59 +0530

    create CodableFeedStoreTests

---
+++ EssentialFeedTests/FeedCase/FeedCache/CodableFeedStoreTests.swift
@@ -0,0 +1,19 @@
+//
+//  CodableFeedStore.swift
+//  EssentialFeedTests
+//
+//  Created by Sai Charan on 04/08/23.
+//
+
+import XCTest
+import EssentialFeed
+
+class CodableFeedStore {
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+
+    }
+}
+
+final class CodableFeedStoreTests: XCTestCase {
+
+}
